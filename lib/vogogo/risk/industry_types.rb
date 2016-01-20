682a500af76276acf4b3c4d6aeda5c3cdf555c46
Marcin Adamczyk <marcin.adamczyk@rst-it.com>
2016-01-20 16:08:32 +0100

    minor fix

--- lib/vogogo/risk/industry_types.rb
+++ lib/vogogo/risk/industry_types.rb
@@ -1,8 +1,10 @@
 module Vogogo
-  module IndustryTypes
-    def get_industry_types
-      connection('industry_types')
-      request_method('get')
+  module Risk
+    module IndustryTypes
+      def get_industry_types
+        connection('industry_types')
+        request_method('get')
+      end
     end
   end
 end
